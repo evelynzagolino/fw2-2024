94765d0fc5a120c476140d37de0bcee42cab8145
evelynzagolino <120220285+evelynzagolino@users.noreply.github.com>
2024-09-30 14:13:14 +0000

    aula do dia 30/09

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,24 @@
 import Footer from "./modulos/footer";
 import Navbar from "./modulos/navbar";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function Index() {
+export default function Index() {
   return <div>
     <Navbar></Navbar>
     Bem-Vindos
-    <Footer></Footer></div>;
+    <Container>
+      <Row>
+        <Col className=" bg-info">1 of 2</Col>
+        <Col className="bg-info-subtle">2 of 2</Col>
+      </Row>
+      <Row>
+        <Col className="bg-primary">1 of 3</Col>
+        <Col className="bg-primary-subtle">2 of 3</Col>
+        <Col className="bg-secondary text-white">3 of 3</Col>
+      </Row>
+    </Container>
+    <Footer></Footer>
+  </div>;
 }
-export default Index;

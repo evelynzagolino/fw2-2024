a62dff7d45ea30dce5b747ec1c22d64bae117c64
evelynzagolino <120220285+evelynzagolino@users.noreply.github.com>
2024-10-07 14:15:44 +0000

    Aula dia 07/10

--- src/pages/modulos/navbar.js
+++ src/pages/modulos/navbar.js
@@ -1,4 +1,19 @@
-function Navbar() {
-    return <div>Menu</div>
-}
-export default Navbar;
+import {Container, Nav, Navbar} from 'react-bootstrap';
+
+export default function Menu() {
+    return (
+        <Navbar expand="lg" className="bg-dark">
+          <Container>
+            <Navbar.Brand href="/" className="text-light">6A</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" >
+              <Nav className="me-auto">
+                <Nav.Link href="#home" className="text-light">Home</Nav.Link>
+                <Nav.Link href="#link" className="text-light">Produtos</Nav.Link>
+
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+    }

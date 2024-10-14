f517d31fe2f8a8db72d752976322a660a4b695c1
evelynzagolino <120220285+evelynzagolino@users.noreply.github.com>
2024-10-14 14:22:38 +0000

    aula dia 14/10

--- src/pages/modulos/navbar.js
+++ src/pages/modulos/navbar.js
@@ -1,19 +1,21 @@
-import {Container, Nav, Navbar} from 'react-bootstrap';
-
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import styles from "@/styles/Navbar.module.css"
 export default function Menu() {
-    return (
-        <Navbar expand="lg" className="bg-dark">
-          <Container>
-            <Navbar.Brand href="/" className="text-light">6A</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav" >
-              <Nav className="me-auto">
-                <Nav.Link href="#home" className="text-light">Home</Nav.Link>
-                <Nav.Link href="#link" className="text-light">Produtos</Nav.Link>
+  return (
+    <Container>
+      <Navbar expand="lg" className={`bg-dark ${styles.navGradiente} px-4 m-0`}>
 
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      );
-    }
+        <Navbar.Brand href="/" className="text-light">6A</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" >
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className="text-light">Home</Nav.Link>
+            <Nav.Link href="#link" className="text-light">Produtos</Nav.Link>
+
+          </Nav > 
+        </Navbar.Collapse >
+
+      </Navbar>
+    </Container>
+  );
+}
